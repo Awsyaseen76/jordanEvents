5b087a14a8795494ee145ddc48ea8695c883c140
Aws <Awsyaseen76@gmail.com>
2018-05-19 02:36:37 +0300

    try 6

--- server/databse.js
+++ server/databse.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
-// var connectionString = 'mongodb://localhost/jordaneventsemifinal'; // for local
+var connectionString = 'mongodb://localhost/jordaneventsemifinal'; // for local
 
 // check if running remotely
-if(process.env.MLAB_USERNAME) { 
+if(process.env.GOOGLE_CALLBACK_URL) { 
         var username = process.env.MLAB_USERNAME;
         var password = process.env.MLAB_PASSWORD;
-        var connectionString = 'mongodb://' + username + ':' + password;
+        connectionString = 'mongodb://' + username + ':' + password;
         connectionString += '@ds147920.mlab.com:47920/heroku_sqjcbg63';
 }
 
