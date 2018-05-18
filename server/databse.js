4301f1b8b8f70a1e56de0aa52700e92fd76e3ab7
Aws <Awsyaseen76@gmail.com>
2018-05-18 23:41:51 +0300

    create database on mlap with heroku

--- server/databse.js
+++ server/databse.js
@@ -3,13 +3,12 @@ var mongoose = require('mongoose');
 var connectionString = 'mongodb://localhost/jordaneventsemifinal'; // for local
 
 // check if running remotely
-// if(process.env.MLAB_USERNAME) { 
-//         var username = process.env.MLAB_USERNAME;
-//         var password = process.env.MLAB_PASSWORD;
-//         connectionString = 'mongodb://' + username + ':' + password;
-//         connectionString += '@ds115340.mlab.com:15340/jordanevents';
-// }
-
+if(process.env.MLAB_USERNAME) { 
+        var username = process.env.MLAB_USERNAME;
+        var password = process.env.MLAB_PASSWORD;
+        connectionString = 'mongodb://' + username + ':' + password;
+        connectionString += '@ds147920.mlab.com:47920/jordanevents';
+}
 mongoose.connect(connectionString);
 
 
