50113f874f385644f9bb892d974c514b9b0a36a3
Aws <Awsyaseen76@gmail.com>
2018-10-29 13:02:51 +0200

    add a special form to register for new event

--- public/events/controllers/eventDetails.controller.client.js
+++ public/events/controllers/eventDetails.controller.client.js
@@ -43,25 +43,55 @@
 
 			function eventRegistration(event, user){
 				console.log(user);
-				// if (!model.loggedUser){
-				// 	model.error1 = 'Please login or sign-up to register on this event';
-				// 	$('html, body').animate({ scrollTop: 0 }, 'slow');
-				// 	return;
-				// } else {
-				// 	var userId = model.loggedUser._id;
-				// 	var eventsList = model.loggedUser.registeredEventsList;
-				// 	for(var e in eventsList){
-				// 		if(eventsList[e]._id === event._id){
-				// 			model.error2 = 'You already registered for this event';
-				// 			return;
-				// 		}
-				// 	}
-				// 	userService
-				// 		.addEventToUserEventsList(event)
-				// 		.then(function (response){
-				// 		$location.url('/userProfile');
-				// 	});
-				// }
+				
+
+				if (!model.loggedUser){
+					model.error1 = 'Please login or sign-up to register on this event';
+					$('html, body').animate({ scrollTop: 0 }, 'slow');
+					return;
+				} else {
+					var userId = model.loggedUser._id;
+					var eventsList = model.loggedUser.registeredEventsList;
+					// for(var e in eventsList){
+					// 	if(eventsList[e]._id === event._id){
+					// 		model.error2 = 'You already registered for this event';
+					// 		return;
+					// 	}
+					// }
+					// userService
+					// 	.addEventToUserEventsList(event)
+					// 	.then(function (response){
+					// 	$location.url('/userProfile');
+					// });
+					if(user.termsAcceptance){
+						$('#eventRegistrationModal').modal('hide');
+					}else{
+						$("#registrationForm").validate({
+						    rules: {
+						      notes: {
+						        required: true,
+						        minlength: 8
+						      },
+						      termsAcceptance: {
+						      	required: true
+						      }
+						    },
+						    messages: {
+						      notes: {
+						        required: "Please enter some data",
+						        minlength: "Your data must be at least 8 characters"
+						      },
+						      termsAcceptance: {
+						      	required: "<b style='color: red;'>Please accept the terms and conditions</b>"
+						      }
+						    }
+						  });
+						// alert('Please accept the terms and conditions');
+						// model.termsError = 'You should read and accept the terms and conditions to register for this event';
+						// $('#registrationForm').css('background-color', 'red');
+					}
+				}
+
 			}
 
 		}
